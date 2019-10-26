2943a0ce0ed859ff60c00fee45132019119b8c2d
qiuqiuf <842792595@qq.com>
2019-10-26 14:29:02 +0800

    集成喜马拉雅sdk成功

--- app/src/main/java/com/himalaya/android/MainActivity.java
+++ app/src/main/java/com/himalaya/android/MainActivity.java
@@ -1,14 +1,44 @@
 package com.himalaya.android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(@Nullable CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    for (Category category : categories) {
+                        Log.d(TAG, "category -- > " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "error code is -- > " + i + " error msg -- >" + s);
+            }
+        });
     }
 }
